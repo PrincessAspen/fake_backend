d2cd8d207f1ab05d6b9833aa3c0af59a6439c579
Aspen Berry <valiantagressor@gmail.com>
2024-10-29 11:52:23 -0400

    Added lots of functionality including logout

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 from sqlmodel import create_engine, SQLModel, Session
-
-DATABASE_URL = "postgresql://postgres.vmoumuxemattlimsxlsk:AGTX4lk4MNOm8VLk@aws-0-us-west-1.pooler.supabase.com:6543/postgres?sslmode=require"
+from config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL, echo=True)
 
